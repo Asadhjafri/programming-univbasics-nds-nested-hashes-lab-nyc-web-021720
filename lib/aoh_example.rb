1bf4de5aa4910ed46bff3bad2e1956906350ff8d
Asad Jafri <asadhjafri@gmail.com>
2020-01-28 06:21:22 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,13 +23,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  family_name = [{ name:  "Don Gately", occupation: "Live-in Staff" },
-  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  { name:  "Pat Monteseian", occupation: "Staff" },
-  { name:  "Kate Gompert", occupation: "None" },
-  { name:  "Bruce Green", occupation: "Fan of Mildred" }]
-
-family_name[1][0]
+aoh[row][key]
 end
 
 
